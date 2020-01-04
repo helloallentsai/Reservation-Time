90fc76edc6716d582261cb64ef7d338041d3f077
Allen Tsai <hello.allen.tsai@gmail.com>
2020-01-03 17:08:47 -0800

    setup load-balancer server

--- server/lb.js
+++ server/lb.js
@@ -1,18 +1,13 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 const { P2cBalancer } = require('load-balancers');
-
+const proxy = require('http-proxy-middleware');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-
-app.use(express.static(__dirname + '/../client/dist'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
@@ -20,16 +15,18 @@ app.use((req, res, next) => {
 });
 
 const proxies = [
-  '172.31.5.1',
-  '172.31.14.222'
+  'http://172.31.5.1:3001',
+  'http://172.31.14.222:3001'
 ];
 
 const balancer = new P2cBalancer(proxies.length);
-const proxy = proxies[balancer.pick()];
+const server = proxies[balancer.pick()];
 
-console.log(proxy);
+console.log(server);
 
-const PORT = 3010;
+app.use(proxy('/', { target: server }));
+
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
